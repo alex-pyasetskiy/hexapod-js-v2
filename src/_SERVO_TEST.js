67150e48788c3d678ebd03f3869d24d56b8e4619
Alexey Pyasetskiy <apyasetskiy@cvent.com>
2023-02-09 10:48:36 +0200

    build22

--- src/_SERVO_TEST.js
+++ src/_SERVO_TEST.js
@@ -1,11 +1,12 @@
 const { Board, Servo } = require("johnny-five")
+const Raspi = require('raspi-io').RaspiIO;
 
 /********************
     You can use this script to test if your servos
     are connected properly as you'd expect.
  ********************/
 
-const board = new Board()
+const board = new Board({io: new Raspi()})
 
 board.on("ready", () => {
     console.log("board connected.")
