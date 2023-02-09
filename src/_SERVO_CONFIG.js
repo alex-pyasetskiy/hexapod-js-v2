79291ed6ede3fe0f24ff0c1f132ac77367e14e3b
Alexey Pyasetskiy <apyasetskiy@cvent.com>
2023-02-09 03:02:24 +0200

    update

--- src/_SERVO_CONFIG.js
+++ src/_SERVO_CONFIG.js
@@ -1,43 +1,44 @@
 // See configurable parameters at: http://johnny-five.io/api/servo/
 
 const controller = "PCA9685"
-const address = "0x41"
+const pca_left = "0x41"
+const pca_right = "0x40"
 
 const servoConfig = {
     rightMiddle: {
-        alpha: { controller, pin: 6, address },
-        beta: { controller, pin: 8, address },
-        gamma: { controller, pin: 7, address },
+        alpha: { controller, pin: 8, pca_right },
+        beta: { controller, pin: 9, pca_right },
+        gamma: { controller, pin: 10, pca_right },
     },
 
     rightFront: {
-        alpha: { controller, pin: 15, address },
-        beta: { pin: 3 },
-        gamma: { pin: 5 },
+        alpha: { controller, pin: 13, pca_right },
+        beta: { controller, pin: 14, pca_right},
+        gamma: {controller, pin: 15, pca_right },
     },
 
     leftFront: {
-        alpha: { controller, pin: 13, address },
-        beta: { controller, pin: 14, address },
-        gamma: { controller, pin: 12, address },
+        alpha: { controller, pin: 0, pca_left },
+        beta: { controller, pin: 1, pca_left },
+        gamma: { controller, pin: 2, pca_left },
     },
 
     leftMiddle: {
-        alpha: { controller, pin: 10, address },
-        beta: { controller, pin: 9, address },
-        gamma: { controller, pin: 11, address },
+        alpha: { controller, pin: 4, pca_left },
+        beta: { controller, pin: 5, pca_left },
+        gamma: { controller, pin: 6, pca_left },
     },
 
     leftBack: {
-        alpha: { controller, pin: 2, address },
-        beta: { controller, pin: 1, address },
-        gamma: { controller, pin: 0, address },
+        alpha: { controller, pin: 13, pca_left },
+        beta: { controller, pin: 14, pca_left },
+        gamma: { controller, pin: 15, pca_left },
     },
 
     rightBack: {
-        alpha: { controller, pin: 5, address },
-        beta: { controller, pin: 4, address },
-        gamma: { controller, pin: 3, address },
+        alpha: { controller, pin: 0, pca_right },
+        beta: { controller, pin: 1, pca_right },
+        gamma: { controller, pin: 2, pca_right },
     },
 }
 
