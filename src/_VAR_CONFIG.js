a0751b088a2edf0aab478ab948fefafbd9665740
Alex Pyasetskiy <alex.pyasetskiy@gmail.com>
2023-02-25 17:13:53 +0200

    server

--- src/_VAR_CONFIG.js
+++ src/_VAR_CONFIG.js
@@ -6,7 +6,7 @@ const SOCKET_SERVER_PORT = 4001
 const SOCKET_SERVER_URL = `http://127.0.0.1:${SOCKET_SERVER_PORT}`
 
 // client urls that are allowed to connect with our robot
-const SOCKET_CLIENT_URLS = ["http://localhost:5000", "http://192.168.31.1000:5000"]
+const SOCKET_CLIENT_URLS = ["http://localhost:5000", "http://192.168.31.100:3000"]
 
 // we don't want to spam too many commands to our robot
 // (in milliseconds)
